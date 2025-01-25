ed2b94f9bfd8807f0a2f3ec595d0e4d6cb1de584
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2025-01-25 22:12:28 +0100

    Start writing solver

--- rust/src/solver.rs
+++ rust/src/solver.rs
@@ -1,6 +1,10 @@
+use std::{collections::HashSet, fmt::UpperExp, ops::{Add, Not}};
+
+use indexmap::{set::Difference, IndexSet};
+
 use crate::Solve;
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 enum Piece {
     Straight,
     Curve,
@@ -15,22 +19,101 @@ enum Instruction {
     Down
 }
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
     Left,
     Up,
     Right,
     Down,
-    Front,
-    Back
+    Forward,
+    Backward
+}
+impl Not for Direction {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Direction::Left => Self::Right,
+            Direction::Up => Self::Down,
+            Direction::Right => Self::Left,
+            Direction::Down => Self::Up,
+            Direction::Forward => Self::Backward,
+            Direction::Backward => Self::Forward,
+        }
+    }
 }
 
-struct Position {
-    pos: (i8, i8, i8),
-    direction: Direction
+impl Direction {
+    fn project(&self, other: Direction) -> Direction {
+        match (*self, other){
+            (Direction::Forward, other) => other,
+            (current, Direction::Forward) => current,
+            (_, Direction::Backward) => panic!("Can't go back"),
+            
+            (Direction::Left | Direction::Right, Direction::Up) => Direction::Up,
+            (Direction::Left | Direction::Right, Direction::Down) => Direction::Down,
+            (Direction::Left, Direction::Right) => Direction::Forward,
+            (Direction::Left, Direction::Left) => Direction::Backward,
+
+            (Direction::Backward, x) => !x,
+            
+            (Direction::Up | Direction::Down, x@ (Direction::Left | Direction::Right)) => x,
+
+            (Direction::Up, Direction::Up) => Direction::Backward,
+            (Direction::Up, Direction::Down) => Direction::Forward,
+
+            (Direction::Right, Direction::Left) => Direction::Forward,
+            (Direction::Right, Direction::Right) => Direction::Backward,
+
+            (Direction::Down, Direction::Down) => Direction::Backward,
+            (Direction::Down, Direction::Up) => Direction::Forward,
+        }
+    }
 }
 
-impl Position {
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct PieceInPosition {
+    pos: Position,
+    direction: Direction,
+    piece: Piece
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Position(i8, i8, i8);
+
+impl Add<(i8, i8, i8)> for Position {
+    type Output = Position;
+
+    fn add(self, rhs: (i8, i8, i8)) -> Self::Output {
+        Position(
+            self.0+rhs.0,
+            self.1+rhs.1,
+            self.2+rhs.2
+        )
+    }
+}
+
+impl PieceInPosition {
+    fn allNextPositions(&self, piece: Piece) -> HashSet<PieceInPosition> {
+        if piece == Piece::Straight {
+            return HashSet::from([PieceInPosition {
+                direction: self.direction,
+                pos: self.pos + (1, 0, 0),
+                piece,
+            }]);
+        }
+
+        let mut result = HashSet::new();
+        for n in [Direction::Left, Direction::Up, Direction::Right, Direction::Down] {
+            result.insert(PieceInPosition {
+                direction: self.direction.project(n),
+                pos: self.pos,
+                piece
+            });
+        }
+        result
+    }
 }
 
 
@@ -44,4 +127,21 @@ pub fn solve(solve: Solve) {
             c => panic!("No such character: {}", c),
         })
         .collect();
+
+    for d in [Direction::Backward, Direction::Down, Direction::Forward, Direction::Left, Direction::Right, Direction::Up] {
+        let mut chain = IndexSet::new();
+        chain.insert(PieceInPosition {
+            piece: Piece::Straight,
+            pos: Position(0, 0, 0),
+            direction: d
+        });
+
+        solveRec(chain, pieces.clone());
+    }
+}
+
+fn solveRec(piecesInPosition: IndexSet<PieceInPosition>, rest: Vec<Piece>) -> Option<IndexSet<PieceInPosition>> {
+ 
+ 
+ None   
 }
